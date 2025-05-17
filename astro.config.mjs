6fb7d90da88b563bbe9960643f29024ca03edd19
kaanguru <kaanguru@mail.ru>
2025-05-17 23:15:26 +0300

    feat: Add Alpine.js support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,38 +2,38 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import alpinejs from '@astrojs/alpinejs';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Routine Trace',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			customCss: [
-				'./src/styles/custom.css',
-			],
-			head: [
-				{
-					tag: 'link',
-					attrs: {
-						rel: 'stylesheet',
-						href: 'https://fonts.googleapis.com/css2?family=Dela+Gothic+One&display=swap',
-					},
-				},
+    integrations: [starlight({
+        title: 'Routine Trace',
+        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+        customCss: [
+            './src/styles/custom.css',
+        ],
+        head: [
+            {
+                tag: 'link',
+                attrs: {
+                    rel: 'stylesheet',
+                    href: 'https://fonts.googleapis.com/css2?family=Dela+Gothic+One&display=swap',
+                },
+            },
 
-			],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+        ],
+        sidebar: [
+            {
+                label: 'Guides',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Example Guide', slug: 'guides/example' },
+                ],
+            },
+            {
+                label: 'Reference',
+                autogenerate: { directory: 'reference' },
+            },
+        ],
+		}), alpinejs()],
 });
