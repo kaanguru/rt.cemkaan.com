fdca6153bcebdeaeaedb14cc51c9370d523c85b5
kaanguru <kaanguru@mail.ru>
2025-05-19 14:16:11 +0300

    fix: update logo and image files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,40 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig, passthroughImageService } from "astro/config";
+import starlight from "@astrojs/starlight";
 
-import alpinejs from '@astrojs/alpinejs';
-
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [starlight({
-        title: 'Routine Trace',
-      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/kaanguru/routinetrace' }],
-        customCss: [
-            './src/styles/custom.css',
-        ],
-        head: [
-            {
-                tag: 'link',
-                attrs: {
-                    rel: 'stylesheet',
-                    href: 'https://fonts.googleapis.com/css2?family=Dela+Gothic+One&display=swap',
-                },
-            },
+  image: {
+    service: passthroughImageService(),
+  },
+  integrations: [
+    starlight({
+      title: "Routine Trace",
+      logo: {
+        src: "./src/assets/logo.svg",
+      },
 
-        ],
-
-		}), alpinejs()],
+      favicon: "/favicon.svg",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/kaanguru/routinetrace",
+        },
+      ],
+      customCss: ["./src/styles/custom.css"],
+      head: [
+        {
+          tag: "link",
+          attrs: {
+            rel: "stylesheet",
+            href: "https://fonts.googleapis.com/css2?family=Dela+Gothic+One&display=swap",
+          },
+        },
+      ],
+    }),
+    alpinejs(),
+  ],
 });
